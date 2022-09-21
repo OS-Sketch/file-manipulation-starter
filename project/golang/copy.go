ae6fab64c72ad0189a13a092c79719a8587a3690
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-09-21 13:53:28 -0400

    chore: Add the copy.go file with the TODO markers.

---
+++ project/golang/copy.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: Add all of the required imports
+
+// TODO: Define any functions used in your program
+
+// TODO: Define a main function that reads in the contents of the source file
+// and writes them to the destination file
